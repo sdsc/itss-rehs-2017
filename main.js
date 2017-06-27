ef8cb2e2d5d078aa0b0ef3723e065bf156a5ba15
trentsikute <tsikute@gmail.com>
2017-06-27 14:37:23 -0700

    Add files via upload
    Upload of SDSC Thermals Project 6/27/17

---
+++ main.js
@@ -0,0 +1,124 @@
+$(document).ready(function() {
+  $.getJSON( "https://graphite.sdsc.edu/render/?width=586&height=309&_salt=1498583446.605&target=hosts.SDSC_CI.ham_sdsc_edu.Inlet_Temp_04h&from=10%3A00_20170627&until=10%3A15_20170627&format=json",
+    function( data ) {  
+      console.log( data );
+      /*$.each( data, function( key, val ) {
+        console.log("key: " + key + "   value:: " + val );
+        console.log(data.length)
+      });*/
+     //});
+//});
+
+var a = data[0]
+//console.log(a)
+var b = a["datapoints"]
+//console.log(b)
+var c = b[0]
+//console.log(c)
+var d = c[0]
+console.log("Current Temperature in Celcius: " + d)
+//this is the temp in Celcius!!
+
+var realTemperature = d*(9/5) + 32
+console.log("Current Temperature in Fareinheight: " + realTemperature)
+
+var greenNblue = Math.floor(255-((255/50)*(realTemperature-60)))
+if (realTemperature >= 120) {
+  var greenNblue = 0;
+} else if (realTemperature <= 60) {
+    var greenNblue = 255;
+} else {
+    greenNblue;
+}
+console.log("gb value: " + greenNblue)
+
+
+
+var x = "rgb";
+var y = "(255,";
+var z = greenNblue
+var f = ","
+var g = ")"
+console.log(x + y + z + f + z + g)
+
+
+$("p").css("color", (x + y + z + f + z + g));
+
+$("table").css("color", (x + y + z + f + z + g));
+    });
+});
+
+$("table").hide();
+$("p").hide();
+
+
+$("img").click(function(){
+    //alert("The photo was clicked.");
+    $("p").fadeIn(1000);
+    $("table").fadeIn(1000);
+    
+});
+
+
+
+
+
+
+
+
+/*var rgbColor = [255, greenNblue, greenNblue]
+console.log(rgbColor)
+//just for reference- doesn't execute anything
+*/
+
+/*
+var hypotheticTemp = (Math.floor((Math.random() * 70)+50))
+//produces random "temperature" between 50 and 120
+console.log("hypothetical temperature:" + hypotheticTemp)
+*/
+
+/*
+$("p").html(<canvas id="myCanvas" width="300" height="300" style="border:1px solid #000000;"></canvas>
+              
+<canvas id="myCanvas" width="200" height="150" style="border:1px solid #000000;">
+</canvas>);
+*/
+
+//rgb(255,255,0));
+
+
+
+/*let RGB = rgb
+let Red = 255
+var Green = greenNblue
+var Blue = greenNblue
+
+console.log(RGB.concat(Red, Green, Blue
+  )) */
+
+
+
+
+//<link rel="stylesheet" href="style.css">
+
+/*var newStyles = document.createElement('style')
+document.head.append(newStyles)
+newStyles.innerHTML = ".my-element {" +
+  "height: " + height + "px;" +
+  "width: 40px;" +
+  "color: #9b806b;" +
+"}"
+*/
+
+
+//console.log(255, greenNblue, greenNblue)
+//this one works too
+
+
+
+/*
+For 60-110 degrees:
+  Formula:
+    rgb (255, Math.floor(255-((255/dif)*(currentTemp-minTemp))), Math.floor(255-((255/dif)*(currentTemp-minTemp))))
+    rgb (255, Math.floor(255-((255/50)*(currentTemp-60))), Math.floor(255-((255/dif)*(currentTemp-50))))
+*/
